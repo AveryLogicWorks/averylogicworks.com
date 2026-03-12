abcf91ea8d8d67ab4823e4ab200b67c6c3ba195c
AveryLogicWorks <averylogicworks@gmail.com>
2026-03-12 15:28:05 -0700

    Add Stripe donation tier buttons

--- assets/site-config.js
+++ assets/site-config.js
@@ -3,8 +3,14 @@ window.AVERY_CONFIG = {
   supportEmail: 'support@averylogicworks.com',
   billingEmail: 'billing@averylogicworks.com',
   stripeLinks: {
-    oneTime: '#',
-    monthly: '#',
+    oneTime10: 'https://buy.stripe.com/test_4gM3coe3FgqB3NK8SP1RC00',
+    oneTime25: 'https://buy.stripe.com/test_8x2cMY6Bd3DP9845GD1RC01',
+    oneTime50: 'https://buy.stripe.com/test_00w00ccZB1vH0By0mj1RC02',
+    oneTime100: 'https://buy.stripe.com/test_dRm4gs4t52zLckgb0X1RC03',
+    monthly10: 'https://buy.stripe.com/test_dRm8wIe3F2zL4ROd951RC04',
+    monthly25: 'https://buy.stripe.com/test_3cIfZaf7J2zLac8glh1RC05',
+    monthly50: 'https://buy.stripe.com/test_8x25kwe3F5LX840d951RC06',
+    monthly100: 'https://buy.stripe.com/test_dRm3cobVx6Q14RO3yv1RC07',
     shop: '#',
     portal: '#'
   },
